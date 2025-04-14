082dbd1cd6f73c32905ea212b0732cfa0037610e
batorgilmm <batorgil.m@nest.edu.mn>
2025-04-14 19:39:30 +0800

    removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import FoodList from "./_components/FoodList";
 export default function Home() {
   console.log("main stack");
 
-  console.log('sub pr 1')
-
-  console.log('sub pr 2')
   return (
     <div className="mx-10 mt-10">
       <CategoriesList />
