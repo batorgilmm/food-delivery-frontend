cb74cf80a198ed9eb02076bf3d38803a0be509af
batorgilmm <batorgil.m@nest.edu.mn>
2025-04-14 19:22:04 +0800

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { CategoriesList } from "./_components/CategoriesList";
 import FoodList from "./_components/FoodList";
 
 export default function Home() {
+
+  console.log('1 stacked head pr')
   return (
     <div className="mx-10 mt-10">
       <CategoriesList />
