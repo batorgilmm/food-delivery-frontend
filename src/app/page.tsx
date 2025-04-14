bb1d3d63225e4f74f099ebc1697a63489c2b0952
batorgilmm <batorgil.m@nest.edu.mn>
2025-04-14 19:42:07 +0800

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { CategoriesList } from "./_components/CategoriesList";
 import FoodList from "./_components/FoodList";
 
 export default function Home() {
-  console.log("main stack");
 
   return (
     <div className="mx-10 mt-10">
